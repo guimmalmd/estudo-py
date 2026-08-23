5d0b803a83e9020324f56012dccf82a209f11d1d
Guilherme <guimmalmeidaads@gmail.com>
2026-08-23 16:29:38 -0300

    feat: No teste mascara_fnaf foi adicionado um remove para evitar repetições  e a troca de tupla para lista

--- tests/mascara_fnaf.py
+++ tests/mascara_fnaf.py
@@ -1,45 +1,56 @@
 import random
 
-encontros = []
-
 acertos = 0 
 erros = 0
 pontuacao = 0
 tentativas = 0
 
-animatronics = ("Freddy", "Toy Chica", "Foxy", "Withered Bonnie", "Golden Freddy", "Puppet")
+animatronics = ["Freddy", "Toy Chica", "Foxy", "Withered Bonnie", "Toy Bonnie", "Puppet"]
 
 while tentativas < 6:
     print("\n=====  MENU FNAF  =====")
     print("""
-        1 - Colocar Máscara
-        2 - Usar Lanterna
-        3 - Dar Corda Na Caixa 
-        4 - Sair
+    1 - Colocar Máscara
+    2 - Usar Lanterna
+    3 - Dar Corda Na Caixa 
+    4 - Sair
     """)
 
     escolhido = random.choice(animatronics)
+    animatronics.remove(escolhido) #Aqui eu removi para que não haja repetição das escolhas
+
     op = int(input(f"CUIDADO! {escolhido} está quase na sua sala!! \nQual opção você vai tomar? Tome cuidado: "))
 
     if op == 4:
         print("Encerrando o sistema...")
-        break
 
     tentativas += 1
-    
+
+    #Se houvesse repetição dava para fazer colocando esses índices:
+
+    # resposta_certa = (
+    #     (escolhido == animatronics[0] and op == 2)
+    #     or (escolhido == animatronics[1] and op == 1)
+    #     or (escolhido == animatronics[2] and op == 2)
+    #     or (escolhido == animatronics[3] and op == 1)
+    #     or (escolhido == animatronics[4] and op == 1)
+    #     or (escolhido == animatronics[5] and op == 3)
+    # ) 
+
     resposta_certa = (
-        (escolhido == animatronics[0] and op == 2)
-        or (escolhido == animatronics[1] and op == 1)
-        or (escolhido == animatronics[2] and op == 2)
-        or (escolhido == animatronics[3] and op == 1)
-        or (escolhido == animatronics[4] and op == 1)
-        or (escolhido == animatronics[5] and op == 3)
+        (escolhido == "Freddy" and op == 2)
+        or (escolhido == "Toy Chica" and op == 1)
+        or (escolhido == "Foxy" and op == 2)
+        or (escolhido == "Withered Bonnie" and op == 1)
+        or (escolhido == "Toy Bonnie" and op == 1)
+        or (escolhido == "Puppet" and op == 3)
     )
 
     if resposta_certa:
         acertos += 1
         pontuacao += 50
-        print("UFA! Você escapou dessa...")
+        print("UFA! Você escapou dessa...") 
+
     else:
         erros += 1
         pontuacao -= 50
